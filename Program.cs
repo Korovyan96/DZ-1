2a3fbd54cdcdc5b53c7f61864b4ed2f9bcdd69b8
Yuliya <korovyan.10@gmail.com>
2022-09-18 21:43:11 +0300

    Ex 2

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine ("Введите первое число");
+int num1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите второе число");
+int num2=Convert.ToInt32(Console.ReadLine());
+if (num1>num2)
+{Console.WriteLine("Большее число " + num1);
+Console.WriteLine("Меньшее число " + num2);
+}
+else
+{Console.WriteLine("Большее число " + num2);
+Console.WriteLine("Меньшее число " + num1);
+}
+

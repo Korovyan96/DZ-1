d5ef728b419f6734665b004d2617a56fab0274a2
Korovyan96 <113104849+Korovyan96@users.noreply.github.com>
2022-10-03 00:00:29 +0300

    Delete Exer6.cs

--- Exer6.cs
+++
@@ -1,6 +0,0 @@
-﻿
-Console.WriteLine("Введите число");
-int num=Convert.ToInt32(Console.ReadLine());
-if (num%2==0)
-{Console.WriteLine ("Число четное "+ num);}
-else {Console.WriteLine ("Число нечетное "+ num);}
